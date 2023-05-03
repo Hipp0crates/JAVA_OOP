51a730134435178c1e66a74dad466ab2c91908d8
Hippocrates <rimo52@gmail.com>
2023-05-03 23:17:54 +0300

    Семинар 06 - правка User.java

--- Seminar06/HomeWork/User.java
+++ Seminar06/HomeWork/User.java
@@ -10,13 +10,4 @@ public class User {
     public String getName() {
         return name;
     }
-
-    public void save() {
-        Persister persister = new Persister(this);
-        persister.save();
-    }
-
-    public void report() {
-        System.out.println("Report for user: " + name);
-    }
 }
